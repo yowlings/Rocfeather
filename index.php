5f54edb91945220f7e2acc2334f698167443ad6b
yowlings@gmail.com <yowlings@gmail.com>
2017-08-23 10:58:53 +0800

    slider beautify

--- index.php
+++ index.php
@@ -2,27 +2,27 @@
 <?php get_header(); ?>
 <div id="page">
 	<div class="content">
+		<?php if (is_home() && !is_paged()) { ?>
+			<?php if($options['mts_featured_slider'] == '1') { ?>
+			<div class="flex-container">
+				<div class="flexslider">
+					<ul class="slides">
+						<?php $my_query = new WP_Query('cat='.$options['mts_featured_cat'].'&posts_per_page=3'); while ($my_query->have_posts()) : $my_query->the_post(); ?>      
+						<li>
+							<?php the_post_thumbnail('slider',array('title' => '')); ?>        
+							<p class="flex-caption">
+								<a href="<?php the_permalink() ?>"><span class="slidertitle"><?php the_title(); ?></span></a>
+								<span class="slidertext"><?php echo excerpt(20); ?></span> 
+							</p>
+						</li>
+						<?php endwhile; ?>
+					</ul>
+				</div>
+			</div>
+			<?php } ?>
+		<?php } ?>
 		<article class="article">
 			<div id="content_box" class="home_page">
-				<?php if (is_home() && !is_paged()) { ?>
-					<?php if($options['mts_featured_slider'] == '1') { ?>
-					<div class="flex-container">
-						<div class="flexslider">
-							<ul class="slides">
-								<?php $my_query = new WP_Query('cat='.$options['mts_featured_cat'].'&posts_per_page=3'); while ($my_query->have_posts()) : $my_query->the_post(); ?>      
-								<li>
-									<?php the_post_thumbnail('slider',array('title' => '')); ?>        
-									<p class="flex-caption">
-										<a href="<?php the_permalink() ?>"><span class="slidertitle"><?php the_title(); ?></span></a>
-										<span class="slidertext"><?php echo excerpt(20); ?></span>            <a class="slider_readmore" href="<?php the_permalink() ?>" rel="nofollow">Read More &rarr; </a>
-									</p>
-								</li>
-								<?php endwhile; ?>
-							</ul>
-						</div>
-					</div>
-					<?php } ?>
-				<?php } ?>
 				<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 					<div class="post excerpt">                        	
 						<header>
@@ -46,7 +46,6 @@
 							<span class="excerpt_overlap"><?php echo excerpt(56);?></span>
 						</div>
 						<span class="cust_category"><?php _e('Category: ','mythemeshop'); ?><?php $category = get_the_category(); echo '<a href="'.get_category_link($category[0]->cat_ID).'">'.$category[0]->cat_name.'</a>';?> </span>
-						<div class="readMore"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>" rel="bookmark"><?php _e('Read More','mythemeshop'); ?></a></div>
 						<div id="hover_line"></div>
 					</div><!--.post excerpt-->
 				<?php endwhile; else: ?>
